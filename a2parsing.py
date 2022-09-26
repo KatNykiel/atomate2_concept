63af20bad9472d4b4ed6bd81543cc94274de85ae
PanayotisManganaris <panos.manganaris@gmail.com>
2022-09-26 18:58:48 -0400

    structure printing

--- a2parsing.py
+++ a2parsing.py
@@ -1,23 +1,33 @@
+"""
+VASP run parsing script
+
+implemented using atomate and jobflow
+
+Objectives:
+1. prepare to backfill database with legacy experiments
+   - collect relaxations + subsequent calculations
+   - fill atomate schema
+     - difference between workflow schema and output schema?
+   - upload to database
+2. write atomic steps to files and populate ml directory on depot
+"""
+
 from atomate2.vasp.drones import VaspDrone
 import os
 import monty
 from pathlib import Path
+
+import pprint
+import json
+
 import subprocess
 from itertools import chain
 
-# drone = VaspDrone()
-# with monty.os.cd('./VASP_runs/Si_test'):
-#     doc = drone.assimilate()
-# drone.get_valid_paths([(r,d,f) for r,d,f in os.walk('.')][2])
-#     print(doc)
-
 drone = VaspDrone()
 
-vaspaths = list(chain.from_iterable(
-  [drone.get_valid_paths(x) for x in os.walk('.') if drone.get_valid_paths(x)]
-))
-
-print(vaspaths)
+vaspaths = chain.from_iterable(
+    [drone.get_valid_paths(x) for x in os.walk('.') if drone.get_valid_paths(x)]
+)
 
 docs = []
 for vaspath in vaspaths:
@@ -25,3 +35,8 @@ for vaspath in vaspaths:
     with monty.os.cd(vaspath):
         docs.append(drone.assimilate())
 
+for doc in docs:
+    for calc in doc.calcs_reversed:
+        for step in calc.dict()['output']['ionic_steps']:
+            print('count')
+            pprint.pprint(step['structure'])
